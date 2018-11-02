54869166ea367881c5b5eee517fc577c8c6478ae
Sushant Sahu <sushant87@gmail.com>
2018-11-02 20:49:57 +0530

    code update

---
+++ proto/args-env-nodejs-tutorial/app.js
@@ -0,0 +1,6 @@
+
+console.log('Arguments to read from command line');
+console.log(process.argv);
+console.log('Enviorment variables to read from process');
+console.log(`PORT = ${process.env.PORT}`);
+console.log(`DEV = ${process.env.DEV}`);
