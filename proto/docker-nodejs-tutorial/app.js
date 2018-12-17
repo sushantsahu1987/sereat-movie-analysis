4bb2add5740a4b9d33dd1a734f38b2ba5286aa92
Sushant Sahu <sushant87@gmail.com>
2018-12-17 18:20:29 +0530

    migration

--- proto/docker-nodejs-tutorial/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/login', (req, resp) => {
-    resp.send({
-        hello: "how are you doing ?"
-    });
-})
-app.listen(3000, () => console.log('server running on port 3000'));
